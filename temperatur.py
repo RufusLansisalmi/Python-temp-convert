fad23bf457e9b0f3278ffee75b8bc4023706dc27
RufusLansisalmi <rufus.lansisalmi@gmail.com>
2025-09-10 09:20:18 +0300

    first commit

---
+++ temperatur.py
@@ -0,0 +1,12 @@
+
+
+def main():
+    print("This is the main function of temperatur.py")
+
+def print_menu():
+   input("enter unit (C, F, K): ")
+
+def get_menu_choice():
+    choice = input("Enter your choice: ")
+    return choice
+def get_unit():
